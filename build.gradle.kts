c0a42a85ce016085b60ffb07c90338bf4d00bc16
MinKyung <omi7894@naver.com>
2023-11-14 22:38:43 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.17"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "studio.aroundhub"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
